9ed8b338fa008dde1f80c43c1ec32999cd1f5c6c
imatrisciano <ivano.matrisciano@canonical.com>
2026-04-28 18:09:30 +0200

    Add markdown parsing in WebUI (#26)
    * Add markdown capability in WebUI
    
    * Bump cli to version `v1.0.0-beta.46`

--- scripts/server-webui.sh
+++ scripts/server-webui.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 port="$(modelctl get webui.http.port)"
 host="$(modelctl get webui.http.host)"
 
-capabilities="text, vision"
+capabilities="text, vision, text:markdown"
 
 exec modelctl serve-webui "$UI_ASSETS_DIRECTORY" --port "$port" --host "$host" --capabilities "$capabilities"
